c10298a027db433bcae8eb4f6d5bbdcbc5b971f1
mairohanhoon <rohantiwari2709@gmail.com>
2023-06-06 22:38:44 +0530

    New learning files created on 06/06/2023

---
+++ src/Primitives.java
@@ -0,0 +1,18 @@
+public class Primitives {
+    public static void main(String[] args) {
+        // Primitive is a data type which you can't break any further
+         int rollno = 64; // 4 bytes
+         char letter = 'r'; //
+         // Character starts with single quotes '' but the string starts with double quotes ""
+        float marks = 98.47f; // for decimal values // 4 bytes
+        double largeDecimalNumbers = 234978235235.24356234; // 8 bytes
+        long largeInteger = 235246245324123L; // 8 bytes
+        boolean check = true; // for true false cases
+
+
+
+
+         // String is not primitive
+         String name = "Rohan";
+    }
+}
