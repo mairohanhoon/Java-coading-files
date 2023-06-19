5020e163bb9a6f076ebbf4a6501fa92c0ba6223f
mairohanhoon <rohantiwari2709@gmail.com>
2023-06-19 15:04:41 +0530

    new files

--- src/Temperature.java
+++ src/Temperature.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class Temperature {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Please enter the temperature in C :");
-        float temC = in.nextFloat();
-
-        float temF = (temC * 9/5) + 32;
-        System.out.println("The temperature in F is : " + temF);
+//        System.out.println("Please enter the temperature in C :");
+//        float temC = in.nextFloat();
+        System.out.println("Enter the string :");
+        String s = in.nextLine();
+//        float temF = (temC * 9/5) + 32;
+//        System.out.println("The temperature in F is : " + temF);
+        System.out.println("The string is : " + s);
     }
 }
 // lecture 5 11:34
